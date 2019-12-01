b9d4a30da22fc01c3286718c4959f90dcd34c1e2
William <wpdcastro@gmail.com>
2019-12-01 16:27:34 -0300

    Atualiza template

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,24 +5,18 @@
    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
    <meta name="description" content="Start your development with a Dashboard for Bootstrap 4.">
    <title> Project G - Hackathon Univem</title>
-   <!-- Favicon -->
-   <link rel="icon" href="./assets/img/brand/favicon.png" type="image/png">
-   <!-- Fonts -->
+   <link rel="icon" href="{{ asset('assets/img/brand/favicon.png') }}" type="image/png">
    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700">
-   <!-- Icons -->
-   <link rel="stylesheet" href="./assets/vendor/nucleo/css/nucleo.css" type="text/css">
-   <link rel="stylesheet" href="./assets/vendor/@fortawesome/fontawesome-free/css/all.min.css" type="text/css">
-   <!-- Page plugins -->
-   <!-- Argon CSS -->
+   <link rel="stylesheet" href="{{ asset('assets/vendor/nucleo/css/nucleo.css') }}" type="text/css">
+   <link rel="stylesheet" href="{{ asset('assets/vendor/@fortawesome/fontawesome-free/css/all.min.css') }}" type="text/css">
    <link rel="stylesheet" href="./assets/css/argon.css?v=1.1.0" type="text/css">
  </head>
 
  <body>
-   <!-- Navabr -->
    <nav id="navbar-main" class="navbar navbar-horizontal navbar-main navbar-expand-lg navbar-dark bg-primary">
      <div class="container">
-       <a class="navbar-brand" href="./pages/dashboards/dashboard.html">
-         <img src="./assets/img/brand/white.png">
+       <a class="navbar-brand" href="#!">
+         <img src="{{ asset('assets/img/brand/white.png')}}">
        </a>
        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-collapse" aria-controls="navbar-collapse" aria-expanded="false" aria-label="Toggle navigation">
          <span class="navbar-toggler-icon"></span>
@@ -32,7 +26,7 @@
            <div class="row">
              <div class="col-6 collapse-brand">
                <a href="./pages/dashboards/dashboard.html">
-                 <img src="./assets/img/brand/blue.png">
+                 <img src="{{ asset('assets/img/brand/blue.png') }}">
                </a>
              </div>
              <div class="col-6 collapse-close">
@@ -80,9 +74,7 @@
        </div>
      </div>
    </nav>
-   <!-- Main content -->
    <div class="main-content">
-     <!-- Header -->
      <div class="header bg-primary pt-5 pb-7">
        <div class="container">
          <div class="header-body">
